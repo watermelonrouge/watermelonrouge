1252e5d6ffa3d1e57ea69a93a4b7c11dbd31861d
watermelonrouge <watermelonrouge@gmail.com>
2017-08-16 00:07:55 -0400

    another test
    another test

---
+++ tableau-wdc/earthquakeWDC.js
@@ -0,0 +1,19 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+myConnector.getSchema = function (schemaCallback) {
+    tableau.log("Hello WDC!");
+};
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
